7e459ed0055e609d1eeece762507e5190a7227bf
Cho Sungha <earl9rey@naver.com>
2023-08-08 14:15:17 +0900

    마이페이지 외부링크 연결 (인스타그램/국가동물등록/문의하기)

--- src/pages/mypage/MyPage.js
+++ src/pages/mypage/MyPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Banner from '../../components/Banner';
 import '../../css/MyPage.css';
 
@@ -28,15 +29,19 @@ function MyPage() {
         </div>
 
         <div id="mypagelist">
+            <Link to="https://www.instagram.com/banjjak.official/?igshid=OGQ5ZDc2ODk2ZA%3D%3D">
             <img src={process.env.PUBLIC_URL + '/assets/icons/insta.png'} id="mypageicons" alt="insta"/>
             <h4 id="mypagename">반짝 인스타그램</h4>
             <img src={process.env.PUBLIC_URL + '/assets/icons/more.png'} id="mypage_more2" alt="more"/><br/>
+            </Link>
         </div>
 
         <div id="mypagelist">
+            <Link to="https://www.animal.go.kr/front/index.do">
             <img src={process.env.PUBLIC_URL + '/assets/icons/national.png'} id="mypageicons" alt="national"/>
             <h4 id="mypagename">국가동물등록</h4>
             <img src={process.env.PUBLIC_URL + '/assets/icons/more.png'} id="mypage_more2" alt="more"/><br/>
+            </Link>
         </div><br/><br/>
 
         <div id="mypagelist">
@@ -52,9 +57,11 @@ function MyPage() {
         </div>
 
         <div id="mypagelist">
+            <Link to="https://open.kakao.com/o/gdi0MYyf">
             <img src={process.env.PUBLIC_URL + '/assets/icons/inquiry.png'} id="mypageicons" alt="inquiry"/>
             <h4 id="mypagename">문의하기</h4>
             <img src={process.env.PUBLIC_URL + '/assets/icons/more.png'} id="mypage_more2" alt="more"/><br/>
+            </Link>
         </div><br/><br/>
 
         <div id="mypagelist">
