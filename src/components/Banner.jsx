949354522cac3295d8902b5c40f277f3a6af0baf
Yeinalee <yeinalee@gmail.com>
2023-08-14 23:19:36 +0900

    [FEAT] 로그인 서버 연결 시도 중

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,44 +1,67 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import '../css/Banner.css';
+import "../css/Banner.css";
 import { Link } from "react-router-dom";
 
 const BtnList = styled.button`
-    color: grey;
-    background : white;
-    border: 0.5px solid #BCBCBC;
-    display : inline-block;
-    width : 97.5px;
-    height : 30px;
-    text-align : center;
-    font-size : 12px;
-    font-weight : bold;
-`
+  color: grey;
+  background: white;
+  border: 0.5px solid #bcbcbc;
+  display: inline-block;
+  // width : 97.5px;
+  width: 25%;
+  height: 30px;
+  text-align: center;
+  font-size: 12px;
+  font-weight: bold;
+`;
 
 const Banner = () => {
-    return (
-        <>
-        <div id="BannerBar">
-            <Link to="/MyPage"><img src={process.env.PUBLIC_URL + '/assets/icons/my.png'} id="my" alt="my"/></Link>
-            <img src={process.env.PUBLIC_URL + '/assets/icons/logo2.png'} id="logo2" alt="logo2"/>
-            <img src={process.env.PUBLIC_URL + '/assets/icons/logout2.png'} id="logout2" alt="logout"/>           
+  return (
+    <>
+      <div id="BannerBar">
+        <Link to="/MyPage">
+          <img
+            src={process.env.PUBLIC_URL + "/assets/icons/my.png"}
+            id="my"
+            alt="my"
+          />
+        </Link>
+        <img
+          src={process.env.PUBLIC_URL + "/assets/icons/logo2.png"}
+          id="logo2"
+          alt="logo2"
+        />
+        <img
+          src={process.env.PUBLIC_URL + "/assets/icons/logout2.png"}
+          id="logout2"
+          alt="logout"
+        />
+      </div>
+      <div id="BannerBtn">
+        <div>
+          <BtnList>반짝소개</BtnList>
+          <BtnList>반짝활동</BtnList>
+          <BtnList>공지사항</BtnList>
+          <BtnList>입양안내</BtnList>
         </div>
-        <div id="BannerBtn">
-            <div>
-                <BtnList>반짝소개</BtnList>
-                <BtnList>반짝활동</BtnList>
-                <BtnList>공지사항</BtnList>
-                <BtnList>입양안내</BtnList>
-                </div>
-                <div>
-                <Link to="/Adoption"><BtnList>입양하기</BtnList></Link>
-                <Link to="/Missing"><BtnList>실종/목격</BtnList></Link>
-                <Link to="/Protection"><BtnList>임시보호</BtnList></Link>
-                <Link to="/Review"><BtnList>입양후기</BtnList></Link>
-                </div>
+        <div>
+          <Link to="/Adoption">
+            <BtnList>입양하기</BtnList>
+          </Link>
+          <Link to="/Missing">
+            <BtnList>실종/목격</BtnList>
+          </Link>
+          <Link to="/Protection">
+            <BtnList>임시보호</BtnList>
+          </Link>
+          <Link to="/Review">
+            <BtnList>입양후기</BtnList>
+          </Link>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Banner;
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,129 +1,189 @@
-import React from 'react';
-import styled from 'styled-components';
-import { UserSelectButtonForShelter } from './UserTypeSelect';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { UserSelectButtonForShelter } from "./UserTypeSelect";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const StyledInput = styled.input`
-    display: block;
-    flex: 1;
-    border: 1px solid #828282;
-    border-radius: 5px 5px 0px 0px;
-    padding: 15px;
-    width: 18rem;
-    font-weight: 600;
-    font-size: 15px;
-    line-height: normal;
+  display: block;
+  flex: 1;
+  border: 1px solid #828282;
+  border-radius: 5px 5px 0px 0px;
+  padding: 15px;
+  width: 18rem;
+  font-weight: 600;
+  font-size: 15px;
+  line-height: normal;
 `;
 
 const StyledPasswordInput = styled.div`
-    position: relative;
-    width: 18rem;
-    margin-left: -2rem; /* 왼쪽으로 이동하는 여백 설정 */
-    
-    input {
-        width: 100%;
-        border: 1px solid #828282;
-        border-top: none;
-        border-radius: 0px 0px 5px 5px;
-        padding: 15px;
-        font-weight: 600;
-        font-size: 15px;
-        line-height: normal;
+  position: relative;
+  width: 18rem;
+  margin-left: -2rem; /* 왼쪽으로 이동하는 여백 설정 */
 
-    }
+  input {
+    width: 100%;
+    border: 1px solid #828282;
+    border-top: none;
+    border-radius: 0px 0px 5px 5px;
+    padding: 15px;
+    font-weight: 600;
+    font-size: 15px;
+    line-height: normal;
+  }
 
-    img {
-        position : absolute;
-        top: 50%;
-        right: -15px;
-        margin: 0;
-        transform: translateY(-50%); /* 세로 중앙 정렬 */
-    }
+  img {
+    position: absolute;
+    top: 50%;
+    right: -15px;
+    margin: 0;
+    transform: translateY(-50%); /* 세로 중앙 정렬 */
+  }
 `;
 
 const CenteredForm = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const CheckboxContainer = styled.div`
-    display: flex;
-    justify-content: flex-start; /* 왼쪽 정렬 */
-    width: 19.5rem; /* 전체 폭 차지 */
-    margin-top: 1rem;
-    margin-bottom: 1rem;  
+  display: flex;
+  justify-content: flex-start; /* 왼쪽 정렬 */
+  width: 19.5rem; /* 전체 폭 차지 */
+  margin-top: 1rem;
+  margin-bottom: 1rem;
 `;
 
 const CustomCheckbox = styled.input`
-    /* 체크박스를 숨기고 커스텀 스타일을 적용 */
-    display: none;
+  /* 체크박스를 숨기고 커스텀 스타일을 적용 */
+  display: none;
 
-    + label {
-        position: relative;
-        padding-left: 30px;
-        padding-right: 15px;
-        cursor: pointer;
-        font-size: 14px;
-        color: #3E3E3E;
-    }
+  + label {
+    position: relative;
+    padding-left: 30px;
+    padding-right: 15px;
+    cursor: pointer;
+    font-size: 14px;
+    color: #3e3e3e;
+  }
 
-    + label:before {
-        content: "";
-        position: absolute;
-        left: 0;
-        top: -3px;
-        width: 20px;
-        height: 20px;
-        background-image: url('${process.env.PUBLIC_URL}/assets/icons/noncheck.png'); /* public 폴더 내 이미지 경로 설정 */
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
+  + label:before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: -3px;
+    width: 20px;
+    height: 20px;
+    background-image: url("${process.env.PUBLIC_URL}/assets/icons/noncheck.png"); /* public 폴더 내 이미지 경로 설정 */
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
 
-    &:checked + label:before {
-        background-image: url('${process.env.PUBLIC_URL}/assets/icons/check.png'); /* public 폴더 내 이미지 경로 설정 */
-    }
+  &:checked + label:before {
+    background-image: url("${process.env.PUBLIC_URL}/assets/icons/check.png"); /* public 폴더 내 이미지 경로 설정 */
+  }
 `;
 
 const SubmitBtn = styled(UserSelectButtonForShelter)`
-    border-radius: 5px;
-    font-size: 18px;
-    font-weight: 600;
-    width: 20rem;
-    padding: 13px;
-`
+  border-radius: 5px;
+  font-size: 18px;
+  font-weight: 600;
+  width: 20rem;
+  padding: 13px;
+`;
 
 const LoginForm = () => {
-    return (
-        <>
-        <CenteredForm>
-            <StyledInput type="text" placeholder="이메일"/>
-            <StyledPasswordInput>
-                <input type="password" placeholder="비밀번호"/>
-                <img src={process.env.PUBLIC_URL + "/assets/icons/visible.png"}/>
-            </StyledPasswordInput>
-            <CheckboxContainer>
-                <CustomCheckbox type="checkbox" id="autoLoginCheck" />
-                <label htmlFor="autoLoginCheck">자동로그인</label>
-                <CustomCheckbox type="checkbox" id="saveIdCheck" />
-                <label htmlFor="saveIdCheck">아이디 저장</label>
-            </CheckboxContainer>
-            <SubmitBtn>로그인</SubmitBtn>
-        </CenteredForm>
-        </>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+  const [rememberId, setRememberId] = useState(false);
+
+  const test = () => {
+    console.log("test");
+  };
+  const handleLogin = async (email, password, rememberMe, rememberId) => {
+    const navigate = useNavigate();
+    try {
+      await axios
+        .post(
+          "http://127.0.0.1:8000/accounts/user/login/",
+          {
+            email: email,
+            password: password,
+            remember_me: rememberMe,
+            remember_id: rememberId,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              // Add any other headers if needed
+            },
+          }
+        )
+        .then((response) => {
+          console.log("response");
+          if (response.status === 200) {
+            // 로그인 성공 시 리다이렉트 또는 상태 업데이트 등 처리
+            console.log("로그인 성공:", response.data);
+            navigate("/Home");
+          }
+        });
+    } catch (error) {
+      // 로그인 실패 시 에러 처리
+      console.log("로그인 실패");
+      setTimeout(() => {
+        console.clear(); // 일정 시간 후에 로그 지우기
+      }, 5000); // 5초 후에 로그 지우기
+      navigate("/Home");
+    }
+  };
+
+  return (
+    <>
+      <CenteredForm>
+        <StyledInput
+          type="text"
+          placeholder="이메일"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <StyledPasswordInput>
+          <input
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <img src={process.env.PUBLIC_URL + "/assets/icons/visible.png"} />
+        </StyledPasswordInput>
+        <CheckboxContainer>
+          <CustomCheckbox
+            type="checkbox"
+            id="autoLoginCheck"
+            checked={rememberMe}
+            onChange={() => setRememberMe(!rememberMe)}
+          />
+          <label htmlFor="autoLoginCheck">자동로그인</label>
+          <CustomCheckbox
+            type="checkbox"
+            id="saveIdCheck"
+            checked={rememberId}
+            onChange={() => setRememberId(!rememberId)}
+          />
+          <label htmlFor="saveIdCheck">아이디 저장</label>
+        </CheckboxContainer>
+        <SubmitBtn
+          onClick={() => handleLogin(email, password, rememberMe, rememberId)}
+        >
+          로그인
+        </SubmitBtn>
+      </CenteredForm>
+    </>
+  );
 };
 
 export default LoginForm;
-export {CustomCheckbox};
-export {CheckboxContainer};
-export {SubmitBtn};
-
-
-
-
-
-
-
-
-
+export { CustomCheckbox };
+export { CheckboxContainer };
+export { SubmitBtn };
