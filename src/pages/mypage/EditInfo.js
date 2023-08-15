f6565ea3d5e929c22ab15812e06452e8e0345322
Yeinalee <yeinalee@gmail.com>
2023-08-16 04:02:38 +0900

    [FEAT] 사용자 정보 변경  구현 중
    이미지 불러오기 기능 안됨

--- src/pages/mypage/EditInfo.js
+++ src/pages/mypage/EditInfo.js
@@ -1,55 +1,195 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import '../../css/EditInfo.css';
-import Banner from '../../components/Banner';
+import axios from "axios";
+import "../../css/EditInfo.css";
+import Banner from "../../components/Banner";
 
 function EditInfo() {
-    return (
-        <>
-        <Banner/>
-        
-        <div id="ei_editphoto">
-            <img src={process.env.PUBLIC_URL + '/assets/icons/editphoto.png'} id="ei_photo1" alt="editphoto"/>
-        </div>
+  const [image, setImage] = useState(null);
+  const [profileImage, setProfileImage] = useState(null);
+  const [nickname, setNickname] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        <div id="ei_div">
+  useEffect(() => {
+    // 페이지가 처음 랜더링될 때 기존 정보를 가져오는 요청
+    const fetchUserInfo = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          "http://127.0.0.1:8000/users/getUserInfo/",
+          {
+            headers: {
+              Authorization: `Token ${token}`,
+            },
+          }
+        );
+        const userInfo = response.data;
+        setImage(userInfo.profile);
+        //console.log(userInfo);
+        setEmail(userInfo.email);
+        setNickname(userInfo.username);
+        setPhoneNumber(userInfo.phone);
+      } catch (error) {
+        console.error("사용자 정보 가져오기 실패:", error);
+        // 에러 처리 로직 추가
+      }
+    };
+    fetchUserInfo();
+  }, []);
+
+  const handleProfileImageUpload = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const formData = new FormData();
+      formData.append("profile", profileImage);
+      const response = await axios.put(
+        "http://127.0.0.1:8000/users/userChangeProfile/",
+        formData,
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+            "Content-Type": "multipart/form-data", // 이미지 업로드를 위한 헤더
+          },
+        }
+      );
+      console.log("프로필 사진 업로드 성공:", response.data);
+      // 업로드 성공 후의 로직 추가
+    } catch (error) {
+      console.error("프로필 사진 업로드 실패:", error);
+      // 에러 처리 로직 추가
+    }
+  };
+  const handleNicknameChange = async () => {
+    console.log(nickname);
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.put(
+        "http://127.0.0.1:8000/users/changeuserinfo/",
+        { username: nickname },
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("!!");
+      console.log("닉네임 변경 성공:", response.data);
+      // 수정 성공 후의 로직 추가
+    } catch (error) {
+      console.error("닉네임 변경 실패:", error);
+      // 에러 처리 로직 추가
+    }
+  };
+  const handlePhoneNumberChange = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.put(
+        "http://127.0.0.1:8000/users/changeuserinfo/",
+        { phone: phoneNumber },
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("연락처 변경 성공:", response.data);
+      // 수정 성공 후의 로직 추가
+    } catch (error) {
+      console.error("연락처 변경 실패:", error);
+      // 에러 처리 로직 추가
+    }
+  };
+
+  return (
+    <>
+      <Banner />
+
+      <div id="ei_editphoto">
+        <img
+          src={
+            image
+              ? image
+              : process.env.PUBLIC_URL + "/assets/icons/editphoto.png"
+          }
+          id="ei_photo1"
+          alt="editphoto"
+        />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => setProfileImage(e.target.files[0])}
+        />
+        <button onClick={handleProfileImageUpload}>프로필 사진 업로드</button>
+      </div>
+
+      <div id="ei_div">
         <div id="ei_nickname">
-            <p id="ei_p1">닉네임</p>
-            <p id="ei_p2">* 닉네임을 설정하면 <span>30일간 변경할 수 없습니다.</span></p>
-            <form>
-                <input type="text" id="ei_form1"/>
-                <button id="ei_btn1">변경하기</button>
-            </form>
+          <p id="ei_p1">닉네임</p>
+          <p id="ei_p2">
+            * 닉네임을 설정하면 <span>30일간 변경할 수 없습니다.</span>
+          </p>
+          <form>
+            <input
+              type="text"
+              id="ei_form1"
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+            />
+            <button id="ei_btn1" onClick={handleNicknameChange}>
+              변경하기
+            </button>
+          </form>
         </div>
 
         <div id="ei_email">
-            <p id="ei_p1">이메일</p>
-            <p id="ei_p2">* 이메일은 변경이 불가능합니다.</p>
-            <form>
-                <p id="ei_form2"></p>
-            </form>
+          <p id="ei_p1">이메일</p>
+          <p id="ei_p2">* 이메일은 변경이 불가능합니다.</p>
+          <form>
+            <input
+              type="email"
+              id="ei_form2"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              disabled
+            />
+          </form>
         </div>
 
         <div id="ei_password">
-            <p id="ei_p1">비밀번호</p>
-            <form>
-                <p id="ei_form3"></p>
-                <Link to="/MyPage/EditInfo/EditPW">
-                <button id="ei_btn2">변경하러 가기</button>
-                </Link>
-            </form>
+          <p id="ei_p1">비밀번호</p>
+          <form>
+            <p
+              id="ei_form3"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></p>
+            <Link to="/MyPage/EditInfo/EditPW">
+              <button id="ei_btn2">변경하러 가기</button>
+            </Link>
+          </form>
         </div>
 
         <div id="ei_phonenum">
-            <p id="ei_p1">연락처</p>
-            <form>
-                <input type="tel" id="ei_form4"/>
-                <button id="ei_btn3">변경하기</button>
-            </form>
+          <p id="ei_p1">연락처</p>
+          <form>
+            <input
+              type="tel"
+              id="ei_form4"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <button id="ei_btn3" onClick={handlePhoneNumberChange}>
+              변경하기
+            </button>
+          </form>
         </div>
-        </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default EditInfo;
