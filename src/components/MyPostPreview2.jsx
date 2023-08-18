d05372ae5dd1b1b79ac6c8b8f65fea9290863acb
Yeinalee <yeinalee@gmail.com>
2023-08-18 19:28:05 +0900

    [FEAT] 내가 쓴 글 수정, 입양 동의 서버 연결

---
+++ src/components/MyPostPreview2.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+const MyPostPreviewContainer = styled.div`
+  flex: 0 0 auto;
+`;
+
+const MyPostPreview2 = ({ post }) => {
+  return (
+    <MyPostPreviewContainer>
+      <div
+        style={{
+          width: "150px",
+          height: "100px",
+          border: "1px solid #BCBCBC",
+          margin: "20px 5px",
+          borderRadius: "10px",
+          display: "inline-block",
+        }}
+      >
+        {post.title}
+        {/* 이미지 가져올때 img 태그 안에 style={{width:'100%', height:'100%', objectFit:'cover', borderRadius : '10px'}} 넣어주기*/}
+      </div>
+    </MyPostPreviewContainer>
+  );
+};
+
+export default MyPostPreview2;
